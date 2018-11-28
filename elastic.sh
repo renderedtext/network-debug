346c74a36888d3423972db7ea015dfedf44af35a
Darko Fabijan <darko@renderedtext.com>
2018-11-28 11:33:11 +0100

    Add elastic test

---
+++ elastic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+start_time=$(date)
+
+for i in {1..10}
+do
+  echo -e "\n\n* network-debug: start test $i at `date`\n\n"
+
+  curl -v https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.1.deb > /dev/null
+
+  echo -e "\n\n* network-debug: end test $i at `date`\n\n"
+done
+
+echo -e "=============================================="
+
+echo "* network-debug: Start time: $start_time, End time: `date`"
