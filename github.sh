b162c4e0bfa9143f0e60eabb3ae718d09d06df86
Darko Fabijan <darko@renderedtext.com>
2018-11-28 16:40:11 +0100

    GitHub

---
+++ github.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+start_time=$(date)
+
+for i in {1..10}
+do
+  echo -e "\n\n* network-debug: start test $i at `date`\n\n"
+
+  git clone https://github.com/rails/rails.git
+
+  echo -e "\n\n* network-debug: end test $i at `date`\n\n"
+done
+
+echo -e "=============================================="
+
+echo "* network-debug: Start time: $start_time, End time: `date`"
