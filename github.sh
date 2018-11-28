e0781d46202bd54b0c7bfd5745e1299549ad27ca
Darko Fabijan <darko@renderedtext.com>
2018-11-28 16:42:41 +0100

    Rm cloned dir

--- github.sh
+++ github.sh
@@ -7,6 +7,7 @@ do
   echo -e "\n\n* network-debug: start test $i at `date`\n\n"
 
   git clone https://github.com/rails/rails.git
+  rm -rf rails
 
   echo -e "\n\n* network-debug: end test $i at `date`\n\n"
 done
